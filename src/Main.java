afe2a3910865b95e0fc5abc9026c94e2c5ea0bc8
mfmsajidh <mohamedsajidh.2018851@iit.ac.lk>
2019-01-22 16:00:06 +0530

    Coded Input Numbers and Displaying Them

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+        String[] squareNumbers = new String[9];
+
+        for (int i=0; i<9 ; i++) {
+
+            boolean notValid;
+
+            do {
+
+                System.out.print("Enter number " + (i+1) + ": ");
+                String squareNumber = scanner.nextLine();
+
+                try{
+                    Integer.parseInt(squareNumber);
+                    notValid = false;
+                    squareNumbers[i] = squareNumber;
+
+                }
+                catch(NumberFormatException ex){
+                    System.out.println("Sorry, I can't accept that. \n" + "It's not a valid number!!! \n" );
+                    notValid = true;
+                }
+            } while (notValid);
+
+        }
+
+        System.out.println();
+
+        for (int i=0; i<squareNumbers.length;) {
+            for (int j=0; j<3; j++) {
+                System.out.print(squareNumbers[i] + "\t");
+                i++;
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Thank you for playing the game!");
+
     }
 }
