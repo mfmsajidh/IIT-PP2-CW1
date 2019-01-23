ea366c45f153d2f4178134b51618ecfa0d5c59ef
mfmsajidh <mohamedsajidh.2018851@iit.ac.lk>
2019-01-23 12:19:40 +0530

    Added Simple Magic Square Function (NIGHTLY)

--- src/Main.java
+++ src/Main.java
@@ -2,89 +2,142 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static boolean getMagicValue(String[] squareNumbers) {
+    private static String[][] getValues() {
+        Scanner scanner = new Scanner(System.in); //Imports the Scanner utility as scanner
 
-        int magicValue;
-        int[] magicValueRow = new int[3];
-        boolean magicValueCheck;
+        String[][] squareNumbers = new String[3][3]; //Declaring and initiating a multidimensional array
+        int count = 1; //Variable to count the number of inputs
 
-        for (int i=0, row=0; i<squareNumbers.length;) {
-            magicValue=0;
+        for (int i=0; i<3 ; i++) {
             for (int j=0; j<3; j++) {
-                magicValue += Integer.parseInt(squareNumbers[i]);
-                magicValueRow[row] = magicValue;
-                i++;
+                boolean notValid; //Variable to validate the inputs from the user
+
+                // Do-While repeats until the user inputs a valid integer
+                do {
+                    System.out.print("Enter number " + (count) + ": ");
+                    String squareNumber = scanner.nextLine();
+
+                    //Exceptions to handle the integer check
+                    try{
+                        Integer.parseInt(squareNumber); //Checks whether the input is an integer
+                        notValid = false;
+                        squareNumbers[i][j] = squareNumber; //If validated without an error, assigns the input into the multidimensional array
+                        count++;
+
+                    }
+                    catch(NumberFormatException ex){
+                        System.out.println("Sorry, I can't accept that." + "\n" + "It's not a valid number!!!" + "\n");
+                        notValid = true;
+                    }
+                } while (notValid); //Exits if notValid is false
             }
-            row++;
+        }
+        return squareNumbers; //returns the values
+    }
+
+    private static void displaySquare(String[][] squareNumbers) {
+        System.out.println();
+
+        //Displays the user input by looping through the multidimensional array
+        for (int i=0; i<squareNumbers.length;i++) {
+            for (int j=0; j<3; j++) {
+                System.out.print(squareNumbers[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean checkRows(String[][] squareNumbers) {
+        int valueRow;
+        int[] valueRows = new int[3];
+        boolean valueCheckRow;
+
+        for (int i=0; i<3; i++) {
+            int value=0;
+            for (int j=0; j<3; j++) {
+                value += Integer.parseInt(squareNumbers[i][j]);
+            }
+            valueRows[i] = value;
         }
 
-        if (magicValueRow[0] == magicValueRow[1]) {
-            magicValue = magicValueRow[0];
-            if (magicValue == magicValueRow[2]) {
-                magicValueCheck = true;
+        if(valueRows[0] == valueRows[1]) {
+            valueRow = valueRows[0];
+            if (valueRow == valueRows[2]) {
+                return valueCheckRow = true;
             }
             else {
-                magicValueCheck = false;
+                return valueCheckRow = false;
             }
-        } else {
-            magicValueCheck = false;
         }
-
-        return magicValueCheck;
+        else {
+            return valueCheckRow = false;
+        }
 
     }
 
-    private static void checkMagicRows(int magicValue, String[] squareNumbers) {
-        
+    private static boolean checkColumns (String[][] squareNumbers) {
+        int valueColumn;
+        boolean valueCheckColumn;
+        int[] valueColumns = new int[3];
+
+        for(int i=0; i<3; i++) {
+            int value=0;
+            for(int j=0; j<3; j++) {
+                value += Integer.parseInt(squareNumbers[j][i]);
+            }
+            valueColumns[i] = value;
+        }
+
+        if(valueColumns[0] == valueColumns[1]) {
+            valueColumn = valueColumns[0];
+            if(valueColumn == valueColumns[2]){
+                return valueCheckColumn = true;
+            }
+            else {
+                return valueCheckColumn = false;
+            }
+        }
+        else {
+            return valueCheckColumn = false;
+        }
+
+    }
+
+    private static boolean checkDiagonals (String[][] squareNumbers) {
+
+        int valueDiagonal;
+        boolean valueCheckDiagonal;
+        int[] valueDiagonals = new int[2];
     }
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String[] squareNumbers = new String[9];
+        //Calls the getValues method and returns the multidimensional array
+        String[][] squareNumbers = getValues();
 
-        for (int i=0; i<9 ; i++) {
+        //Calls the displaySquare method
+        displaySquare(squareNumbers);
 
-            boolean notValid;
+        //Calls the checkRows method
+        boolean valueCheckRow = checkRows(squareNumbers);
 
-            do {
+        //Calls the checkColumns method
+        boolean valueCheckColumn = checkColumns(squareNumbers);
 
-                System.out.print("Enter number " + (i+1) + ": ");
-                String squareNumber = scanner.nextLine();
+        //Calls the checkDiagonals method
+        boolean valueCheckDiagonal = checkDiagonals(squareNumbers);
+        
 
-                try{
-                    Integer.parseInt(squareNumber);
-                    notValid = false;
-                    squareNumbers[i] = squareNumber;
 
-                }
-                catch(NumberFormatException ex){
-                    System.out.println("Sorry, I can't accept that. \n" + "It's not a valid number!!! \n");
-                    notValid = true;
-                }
-            } while (notValid);
 
-        }
-
-        System.out.println();
-
-        for (int i=0; i<squareNumbers.length;) {
-            for (int j=0; j<3; j++) {
-                System.out.print(squareNumbers[i] + "\t");
-                i++;
-            }
-            System.out.println();
-        }
-
-//        int magicValue = getMagicValue(squareNumbers);
+//        int magicValue = checkRows(squareNumbers);
 //        System.out.println(magicValue);
 //        checkMagicRows(magicValue, squareNumbers);
 
-        System.out.println("\n" + "Is Magic Square: "+ getMagicValue(squareNumbers));
-        System.out.println("Is Lo Shu Square: ");
+//        System.out.println("\n" + "Is Magic Square: "+ checkRows(squareNumbers));
+//        System.out.println("Is Lo Shu Square: ");
 
-        System.out.println();
-        System.out.println("Thank you for playing the game!");
+        System.out.println("\n" + "Thank you for playing the game!");
 
     }
 }
